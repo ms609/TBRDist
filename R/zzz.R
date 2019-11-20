d72a99b4f1b5e76ecd9d299fccba9e24d6d781f0
Martin Smith <martins@gmail.com>
2019-11-20 11:44:23 +0000

    Metadata

--- R/zzz.R
+++ R/zzz.R
@@ -8,18 +8,17 @@ release_questions <- function() {
   c(
     "Have you cleared GitHub issues for this release milestone?",
     "Is the code free of #TODOs?",
-    "Have you checked the Vignettes for sanity?",
+    "Have you checked the Vignette for sanity?",
     "Have you refreshed the package meta with codemetar::write_codemeta()?",
-    "Have you pkgdown::build_site()d (if not done by Travis)?",
     "Have you updated the version number in .zenodo.json, NEWS & DESCRIPTION?"
   )
 }
 
 #rhub::check_on_windows()
-#check_with_rdevel() # redundifies check_on_debian()
-#check_on_ubuntu()
-#check_on_fedora()
-#check_on_centos()
-#check_with_valgrind() # runs the build and check on Linux, in valgrind to find memory leaks and pointer errors.
-#check_with_sanitizers() # runs all package package tests, examples and vignettes with Address Sanitizer and Undefined Behavior Sanitizer.
+#rhub::check_with_rdevel() # redundifies check_on_debian()
+#rhub::check_on_ubuntu()
+#rhub::check_on_fedora()
+#rhub::check_on_centos()
+#rhub::check_with_valgrind() # runs the build and check on Linux, in valgrind to find memory leaks and pointer errors.
+#rhub::check_with_sanitizers() # runs all package package tests, examples and vignettes with Address Sanitizer and Undefined Behavior Sanitizer.
 #list_my_checks() # list_package_checks
