88002e6c4d2e7ff7503261e469afe8c083346993
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2026-03-19 08:35:26 +0000

    memcheck/tests.R: devtools::test() -> testthat::test_local()

--- memcheck/tests.R
+++ memcheck/tests.R
@@ -1,5 +1,6 @@
-# Code to be run with  
-#   R -d "valgrind --tool=memcheck --leak-check=full" --vanilla < memcheck/tests.R
+# Code to be run with
+#   R -d "valgrind --tool=memcheck --leak-check=full --track-origins=yes \
+#     --errors-for-leak-kinds=definite --suppressions=suppressions.supp \
+#     --error-exitcode=1" --vanilla < memcheck/tests.R
 # First build and install the package.
-library("TBRDist")
-devtools::test()
+testthat::test_local()
